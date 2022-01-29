35e5129bc9c1f39801e1cafbf74c8a1d6fae0a15
Megan Nadkarni <megan.nadkarni@gmail.com>
2022-01-28 21:32:26 -0800

    boilerplate and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+module.exports = {
+  entry:[path.join(__dirname, 'client', 'index.js'), path.join(__dirname, 'client', 'style.scss')],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        }
+      },
+      {
+        test: /.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: path.join(__dirname, 'client', 'index.html'),
+    })
+  ],
+  devServer: {
+    static: {
+      publicPath: '/',
+      directory: path.resolve(__dirname, 'dist'),
+    },
+  }
+}
